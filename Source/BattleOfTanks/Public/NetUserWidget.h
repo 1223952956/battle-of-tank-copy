6ffbb0e6c4da0efd533281c8c35734fb80195b19
任雨辰 <yuchen.ren@appshahe.com>
2021-12-21 20:48:23 +0800

    test game mode

---
+++ Source/BattleOfTanks/Public/NetUserWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "NetUserWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEOFTANKS_API UNetUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
